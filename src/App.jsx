a2e35e1f66d2ef09d0edfd59e3c879fb92acc196
JorgeCarlotto <jorgemcarlotto@gmail.com>
2022-03-06 17:08:24 -0300

    components and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import Header from './components/header/Header.jsx'
+import Nav from './components/nav/Nav.jsx'
+import About from './components/about/About.jsx'
+import Experience from './components/experience/Experience.jsx'
+import Services from './components/services/Services.jsx'
+import Porfolio from './components/porfolio/Porfolio.jsx'
+import Testimonials from './components/testimonials/Testimonials.jsx'
+import Contact from './components/contact/Contact.jsx'
+import Footer from './components/footer/Footer.jsx'
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Services />
+      <Porfolio />
+      <Testimonials />
+      <Contact />
+      <Footer /> 
+    </>
   )
 }
 
---
+++ src/components/testimonials/Testimonials.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Testimonials.CSS'
+
+const Testimonials = () => {
+  return (
+    <div>Testimonials</div>
+  )
+}
+
+export default Testimonials
