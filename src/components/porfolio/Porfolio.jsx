ecd7ef338282bce0d7601cf546539a626a4c2d72
JorgeCarlotto <jorgemcarlotto@gmail.com>
2022-03-12 16:16:03 -0300

    services ok, porfolio test

--- src/components/porfolio/Porfolio.jsx
+++ src/components/porfolio/Porfolio.jsx
@@ -1,9 +1,80 @@
 import React from 'react'
 import './Porfolio.css'
+import Img1 from '../../assets/saya sayayin.jpg'
+import Img2 from '../../assets/saga frezzer.jpg'
+import Img3 from '../../assets/saga androides.jpg'
+import Img4 from '../../assets/saga bu.jpg'
+import Img5 from '../../assets/saga super.jpg'
+
+
 
 const Porfolio = () => {
   return (
-    <section id="porfolio">Porfolio</section>
+    <section id="porfolio">
+      <h5>Mi trabajo reciente</h5>
+      <h2>Porfolio</h2>
+      <div className="container porfolio__container">
+        {/* article1 */}
+        <article className="porfolio__item">
+          <div className="porfolio__item-image">
+            <img src={Img1} alt="asd1" />
+          </div>
+          <h3>titulo del proyecto</h3>
+          <div className="porfolio__item-cta">
+          <a href="https://github.com/JorgeCarlotto" className='btn' target="_blank"> Github</a>
+          <a href="https://dribbble.com/" className='btn btn-primary' target="_blank">Mira la Demo</a>
+          </div>
+        </article> 
+
+        {/* article2 */}
+        <article className="porfolio__item">
+          <div className="porfolio__item-image">
+            <img src={Img2} alt="asd1" />
+          </div>
+          <h3>titulo del proyecto</h3>
+          <div className="porfolio__item-cta">
+          <a href="https://github.com/JorgeCarlotto" className='btn' target="_blank"> Github</a>
+          <a href="https://dribbble.com/" className='btn btn-primary' target="_blank">Mira la Demo</a>          
+            </div>
+        </article> 
+
+        {/* article3 */}
+        <article className="porfolio__item">
+          <div className="porfolio__item-image">
+            <img src={Img3} alt="asd1" />
+          </div>
+          <h3>titulo del proyecto</h3>
+          <div className="porfolio__item-cta">
+          <a href="https://github.com/JorgeCarlotto" className='btn' target="_blank"> Github</a>
+          <a href="https://dribbble.com/" className='btn btn-primary' target="_blank">Mira la Demo</a>           
+            </div> 
+        </article> 
+
+        {/* article4 */}
+        <article className="porfolio__item">
+          <div className="porfolio__item-image">
+            <img src={Img4} alt="asd1" />
+          </div>
+          <h3>titulo del proyecto</h3>
+          <div className="porfolio__item-cta">
+          <a href="https://github.com/JorgeCarlotto" className='btn' target="_blank"> Github</a>
+          <a href="https://dribbble.com/" className='btn btn-primary' target="_blank">Mira la Demo</a>          
+            </div>
+        </article> 
+
+        {/* article5 */}
+        <article className="porfolio__item">
+          <div className="porfolio__item-image">
+            <img src={Img5} alt="asd1" />
+          </div>
+          <h3>titulo del proyecto</h3>
+          <div className="porfolio__item-cta">
+          <a href="https://github.com/JorgeCarlotto" className='btn' target="_blank"> Github</a>
+          <a href="https://dribbble.com/" className='btn btn-primary' target="_blank">Mira la Demo</a>           
+            </div>
+        </article> 
+      </div>
+    </section>
   )
 }
 
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,8 +1,89 @@
 import React from 'react'
+import './Services.css'
+ import {BiCircle} from 'react-icons/bi'
 
 const Services = () => {
   return (
-    <section id="services">Services </section>
+    <section id="services">
+    <h5>Que puedo ofrecer...</h5>
+    <h2>Servicios</h2>
+     
+    <div className="container services__container">
+      <article className="service">
+        <div className="service__head">
+          <h3>Servicio 1</h3>
+        </div>
+      <ul className="service__list">
+        <li>
+          <BiCircle className="service__list-icon" />
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.  </p>
+        </li>
+        <li>
+          <BiCircle className="service__list-icon" />
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.  </p>
+        </li>
+        <li>
+          <BiCircle className="service__list-icon" />
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.  </p>
+        </li>
+        <li>
+          <BiCircle className="service__list-icon" />
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.  </p>
+        </li>
+      </ul>
+      </article>
+      {/* Service 2 */}
+      <article className="service">
+        <div className="service__head">
+          <h3>Servicio 2</h3>
+        </div>
+      <ul className="service__list">
+        <li>
+          <BiCircle className="service__list-icon" />
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.  </p>
+        </li>
+        <li>
+          <BiCircle className="service__list-icon" />
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.  </p>
+        </li>
+        <li>
+          <BiCircle className="service__list-icon" />
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.  </p>
+        </li>
+        <li>
+          <BiCircle className="service__list-icon" />
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.  </p>
+        </li>
+      </ul>
+      </article>
+      {/* Service 3 */}
+      <article className="service">
+        <div className="service__head">
+          <h3>Servicio 3</h3>
+        </div>
+      <ul className="service__list">
+        <li>
+          <BiCircle className="service__list-icon" />
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.  </p>
+        </li>
+        <li>
+          <BiCircle className="service__list-icon" />
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.  </p>
+        </li>
+        <li>
+          <BiCircle className="service__list-icon" />
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.  </p>
+        </li>
+        <li>
+          <BiCircle className="service__list-icon" />
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.  </p>
+        </li>
+      </ul>
+      </article>
+
+    </div>
+    
+    </section>
   )
 }
 
